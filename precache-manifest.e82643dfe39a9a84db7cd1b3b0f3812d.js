b0a9d7b65a2a39016b256865ad2f9fe29f6647c6
Lucas <lebra448@gmail.com>
2020-05-26 20:53:24 -0400

    Updates

---
+++ precache-manifest.e82643dfe39a9a84db7cd1b3b0f3812d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e6ad58bb1d7150457ce61db3811341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "335dec1c67ab1e4d5c06",
+    "url": "/static/css/main.7d0314cc.chunk.css"
+  },
+  {
+    "revision": "c401f4ddb725821ca602",
+    "url": "/static/js/2.2fb493ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2fb493ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335dec1c67ab1e4d5c06",
+    "url": "/static/js/main.fd485a27.chunk.js"
+  },
+  {
+    "revision": "2b4feade24986e7c1006",
+    "url": "/static/js/runtime-main.daa59b7a.js"
+  }
+]);
